2a3855f5dad549e0fd149bfda2d63cee0f27597e
Mahmutov Azat <cudraviymalay@gmail.com>
2025-02-02 21:36:20 +0300

    склонировал прошлый проект с mysql на докер-контейнере

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,23 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import ru.kata.spring.boot_security.demo.models.User;
+
+import java.security.Principal;
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getAllUsers();
+
+    void save(User user);
+
+    User getOne(Long id);
+
+    public void createUser(User user);
+
+    public void updateUser(User user);
+
+    void delete(Long id);
+
+    public User oneUser(Principal principal);
+}
